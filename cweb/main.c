71f872efcff1096b1ccefd510b5cde8522e2730e
Oleg Yashniy <oleh.yashnyi@gmail.com>
2023-03-04 01:01:29 +0200

    Initial Commit

---
+++ cweb/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  cweb
+//
+//  Created by Oleh Yashnyi on 04.03.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
